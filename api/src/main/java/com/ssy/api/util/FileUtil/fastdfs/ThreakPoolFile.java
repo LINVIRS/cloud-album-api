57c7832c8a0514fe927fd983b18d737595d2c50f
wangfeng <1244353765@qq.com>
2021-04-13 22:03:35 +0800

    相册详情接口，修改相册接口，删除相册接口

--- api/src/main/java/com/ssy/api/util/FileUtil/fastdfs/ThreakPoolFile.java
+++ api/src/main/java/com/ssy/api/util/FileUtil/fastdfs/ThreakPoolFile.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
-import sun.net.www.content.image.png;
 
 import javax.annotation.Resource;
 import java.io.*;
