cd8d4fc5f1c88a6e63e81643b821481e456fad03
ruerjiefei <1294898797@qq.com>
2021-04-29 11:36:52 +0800

    合并代码

--- utils/src/main/java/com/ssy/api/utils/FileUploaderUtil.java
+++ utils/src/main/java/com/ssy/api/utils/FileUploaderUtil.java
@@ -1,2 +1,38 @@
-package com.ssy.api.utils;public class FileUploaderUtil {
+package com.ssy.api.utils;
+
+import io.minio.MinioClient;
+
+import java.io.File;
+
+public class FileUploaderUtil {
+    public static Boolean uploadVideo(String path) {
+        MinioClient minioClient = null;
+        try {
+            minioClient = new MinioClient("http://121.5.235.153:9050", "ssyoss", "ssy123456");
+            // 检查存储桶是否已经存在
+            boolean isExist = minioClient.bucketExists("ssy-oss");
+            if (!isExist) {
+                // 创建一个名为asiatrip的存储桶，用于存储照片的zip文件。
+                minioClient.makeBucket("ssy-oss");
+            }
+            File file = new File(path);
+            // 使用putObject上传一个文件到存储桶中。
+            minioClient.putObject("ssy-oss", file.getName(), path, null);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static String getVideo(String path) {
+        MinioClient minioClient = null;
+        try {
+            minioClient = new MinioClient("http://121.5.235.153:9050", "ssyoss", "ssy123456");
+            return minioClient.getObjectUrl("ssy-oss", new File(path).getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
